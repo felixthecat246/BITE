97a79c0ea75d9298e101288a2bf49a1a00129f0b
felixthecat246 <chrisfelix2001@gmail.com>
2023-05-18 04:04:25 -0400

    initlalization of classes

---
+++ src/StandardDriver.java
@@ -0,0 +1,7 @@
+public class StandardDriver
+{
+    public static void main(String[] args)
+    {
+        System.out.println("");
+    }
+}
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,1 +1,19 @@
-public
+public Transaction
+
+{
+    private String name;
+    private double amount;
+
+    public Transaction(String name, double amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
